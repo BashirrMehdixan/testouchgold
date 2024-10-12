8464f8f0fa69ac67eff281cd10a476dca7eff0c9
Bashirr <beshirr096@gmail.com>
2024-10-12 14:07:34 -0700

    seo page added to admin panel

--- app/Filament/Resources/SeoResource/Pages/ListSeos.php
+++ app/Filament/Resources/SeoResource/Pages/ListSeos.php
@@ -9,4 +9,11 @@ use Filament\Resources\Pages\ListRecords;
 class ListSeos extends ListRecords
 {
     protected static string $resource = SeoResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            Actions\CreateAction::make(),
+        ];
+    }
 }
